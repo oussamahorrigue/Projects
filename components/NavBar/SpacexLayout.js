4154a8befd1d598c2c8cbb135ee76476efd70c75
Oussama Horrigue <horrigueouss@gmail.com>
2022-01-17 13:26:03 +0100

    fixing the navbar

--- components/NavBar/SpacexLayout.js
+++ components/NavBar/SpacexLayout.js
@@ -3,42 +3,22 @@ import Link from "next/link";
 
 const Layout = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link href="/">
-                <a className="nav-link " aria-current="page">
-                  SpaceX
-                </a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/videoBrowser">
-                <a className="nav-link">videoBrowser</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="wikipedia">
-                <a className="nav-link">Wikipedia</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <>
+      <div className="ui pointing menu">
+        <Link href="/">
+          <a className="active item">SpaceX</a>
+        </Link>
+        <Link href="/videoBrowser">
+          <a className="item">videoBrowser</a>
+        </Link>
+        <Link href="wikipedia">
+          <a className="item">Wikipedia</a>
+        </Link>
       </div>
-    </nav>
+      <div className="ui segment">
+        <p></p>
+      </div>
+    </>
   );
 };
 export default Layout;
--- components/NavBar/WikiLayout.js
+++ components/NavBar/WikiLayout.js
@@ -4,51 +4,36 @@ import { Container, Title } from "./style-content-navbar";
 
 const Layout = ({ setTerm }) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link href="/">
-                <a className="nav-link " aria-current="page">
-                  SpaceX
-                </a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/videoBrowser">
-                <a className="nav-link">videoBrowser</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="wikipedia">
-                <a className="nav-link">Wikipedia</a>
-              </Link>
-            </li>
-          </ul>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search wikipedia"
-              aria-label="Search"
-              onChange={(e) => setTerm(e.target.value)}
-            />
-          </form>
+    <>
+      <div className="ui pointing menu">
+        <Link href="/">
+          <a className="active item">SpaceX</a>
+        </Link>
+        <Link href="/videoBrowser">
+          <a className="item">videoBrowser</a>
+        </Link>
+        <Link href="wikipedia">
+          <a className="item">Wikipedia</a>
+        </Link>
+        <div className="right menu">
+          <div className="item">
+            <form>
+              <div className="ui transparent icon input">
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  onChange={(e) => setTerm(e.target.value)}
+                />
+                <i className="search link icon"></i>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </nav>
+      <div className="ui segment">
+        <p></p>
+      </div>
+    </>
   );
 };
 export default Layout;
